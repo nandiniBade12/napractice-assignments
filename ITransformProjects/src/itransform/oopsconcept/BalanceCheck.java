2b9a0db2126f5905d5e86c9cd84e17fb4369fd48
nandiniBade12 <85821013+nandiniBade12@users.noreply.github.com>
2023-07-25 13:36:27 +0530

    Update BalanceCheck.java

--- ITransformProjects/src/itransform/oopsconcept/BalanceCheck.java
+++ ITransformProjects/src/itransform/oopsconcept/BalanceCheck.java
@@ -1,38 +1,86 @@
 package itransform.oopsconcept;
 
-public class BalanceCheck {
+class AccountHolder{
 	
+	private String name;
 	private double balance;
+	
+	public AccountHolder() {
+		super();
+	}
+
+	public AccountHolder(String name, double balance) {
+		super();
+		this.name = name;
+		this.balance = balance;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
+	
+}
+
+class SavingsAccount extends AccountHolder{
 	
 	private double fixed_deposit;
 	
-	private double cash_credit;
-	
-	public BalanceCheck() {
+	public SavingsAccount() {
 		super();
 	}
 
-	public BalanceCheck(double balance, double fixed_deposit, double cash_credit) {
+	public SavingsAccount(String name, double balance) {
+		super(name, balance);
+	}
+	
+	public SavingsAccount(double fixed_deposit) {
 		super();
-		this.balance = balance;
 		this.fixed_deposit = fixed_deposit;
-		this.cash_credit = cash_credit;
 	}
 
-
-
-	public double getBalance1(double fixed_deposit) {
-		return fixed_deposit;
+	@Override
+	public double getBalance() {
+		return super.getBalance() + fixed_deposit;
 	}
 	
-	public double getBalance(double cash_credit) {
-		return cash_credit;
+}
+
+class CurrentAccount extends AccountHolder{
+	
+	private double credit_cash;
+
+	public CurrentAccount() {
+		super();
 	}
+
+	public CurrentAccount(String name, double balance) {
+		super(name, balance);
+	}
+	
+	public CurrentAccount(double credit_cash) {
+		super();
+		this.credit_cash = credit_cash;
+	}
+
+	@Override
+	public double getBalance() {
+		return super.getBalance() + credit_cash;
+	}
+	
+}
+public class BalanceCheck {
 
 	public static void main(String[] args) {
 		
+		double total_balance=0;
 		
-		double total_amount=0;
+		AccountHolder a1= new AccountHolder("Suresh",988455.45);
+		AccountHolder savings = new SavingsAccount(500550);
+		AccountHolder current = new CurrentAccount(351000);
+		
+		total_balance = savings.getBalance()+current.getBalance()+a1.getBalance();
+		
+		System.out.println("Total bank balance of account holder a1 : " + total_balance);
 		
 	}
 
